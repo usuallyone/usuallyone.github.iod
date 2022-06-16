35396fc5791b68b7f329756f31315ad8bc8ee1a7
usuallyone <107552421+usuallyone@users.noreply.github.com>
2022-06-15 22:21:32 -0500

    tarea

---
+++ app.js
@@ -0,0 +1,14 @@
+function ir(){
+    var c=123;
+    var u="angel";
+
+    if(document.form.password.value==c && document.form.usuario.value==u){
+
+    alert("bienvenido");
+
+    window.location="inicio1.html";
+    }
+else{
+    alert("porfavor ingresa,usuario y contraseña correctos");
+}
+}
